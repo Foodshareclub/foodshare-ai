e07aae7dbced844fa2dbb16ff406dcb31a4c8d9a
organicnz <tamerlanium@gmail.com>
2026-01-14 18:56:45 -0800

    Update scan frequency to 2 hours

--- supabase/migrations/20260114210000_scan_frequency.sql
+++ supabase/migrations/20260114210000_scan_frequency.sql
@@ -1,8 +1,8 @@
--- Update scan frequency to every 6 hours
+-- Update scan frequency to every 2 hours
 SELECT cron.unschedule('scan-repos');
 SELECT cron.schedule(
   'scan-repos',
-  '0 */6 * * *',
+  '0 */2 * * *',
   $$SELECT net.http_post(
     url := 'https://mojsubkqjdruhpxbzgme.supabase.co/functions/v1/scan-repos',
     headers := '{"Authorization": "Bearer foodshare-cron-2026", "Content-Type": "application/json"}'::jsonb,
